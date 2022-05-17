32b75fd35c1b12a3858cbe7648bdbb9177ae7b49
Kleiton Lucas <kleitonlucs@gmail.com>
2022-05-17 12:43:35 -0300

    Commit inicial.

---
+++ api-financial-control/src/main/java/com/api/financial/ApiFinancialControlApplication.java
@@ -0,0 +1,13 @@
+package com.api.financial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFinancialControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFinancialControlApplication.class, args);
+	}
+
+}
